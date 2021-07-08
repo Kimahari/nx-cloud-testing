c34142a39041e7e35f74789c00c5cf448cd01884
Herman Esterhuizen <1herman.esterhuizen@gmail.com>
2021-07-08 23:08:24 +0200

    Updates

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 rimraf ./node_modules/.cache
-NX_BRANCH=testing11 npx nx run-many --target=lint --all
-NX_BRANCH=testing11 npx nx run-many --target=test --all
-NX_BRANCH=testing11 npx nx run-many --target=build --all
+NX_BRANCH=testing11 npx nx run-many --target=build --all --parallel --max-parallel=3
+NX_BRANCH=testing11 npx nx run-many --target=test --all --parallel --max-parallel=3
+NX_BRANCH=testing11 npx nx run-many --target=lint --all --parallel --max-parallel=3
